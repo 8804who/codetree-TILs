98e2a165e66cf505cdc166597a2453ac8dd2fd14
README <83859931+8804who@users.noreply.github.com>
2024-01-31 14:48:39 +0900

    Title: 초대장과 번호표, Time: 337ms, Memory: 34MB, Status: Passed - CodeTree

--- 240131/초대장과 번호표/invitation-and-number-tag.py
+++ 240131/초대장과 번호표/invitation-and-number-tag.py
@@ -6,14 +6,16 @@ for _ in range(G):
     members = set(list(map(int, input().split()))[1:])
     groups.append(members)
 
-invite = set()
+answer = 0
 stack = [1]
 while stack:
+    invite = set()
     while stack:
         invite.add(stack.pop())
+    answer += len(invite)
     for group in groups:
         group -= invite
         if len(group) == 1:
             stack.append(tuple(group)[0])
     
-print(len(invite))
+print(answer)
