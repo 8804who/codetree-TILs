08f17e05e5ea0de29d32a174898fc5cad3f7f3a4
README <83859931+8804who@users.noreply.github.com>
2024-02-05 15:51:12 +0900

    Title: 정원 입장은 선착순, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240205/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.cpp
+++ 240205/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.cpp
@@ -1,20 +1,40 @@
-#include <algorithm>
 #include <iostream>
+#include <tuple>
 #include <vector>
+#include <queue>
 
 using namespace std;
+
+struct Comparator {
+    bool operator()(tuple<int, int, int>& t1, tuple<int, int, int>& t2) {
+        if (get<0>(t1) == get<0>(t2)) return get<2>(t1) < get<2>(t2);
+        else return get<0>(t1) < get<2>(t2);
+     }
+ };
 
 int main() {
     int N, a, t;
     scanf("%d",&N);
 
-    vector<pair<int, int>> arr;
-
+    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, Comparator> pq;
     for (int i=0;i<N;i++)
     {
         scanf("%d %d",&a, &t);
-        arr.push_back(a, t);
-        printf("%d, %d", arr[i]);
+        pq.push(make_tuple(a, t, i));
     }
+
+    printf("%d %d %d\n", get<0>(pq.top()),get<1>(pq.top()),get<2>(pq.top()));
+    pq.pop();
+    printf("%d %d %d\n", get<0>(pq.top()),get<1>(pq.top()),get<2>(pq.top()));
+    pq.pop();
+    printf("%d %d %d\n", get<0>(pq.top()),get<1>(pq.top()),get<2>(pq.top()));
+    pq.pop();
+    printf("%d %d %d\n", get<0>(pq.top()),get<1>(pq.top()),get<2>(pq.top()));
+    pq.pop();
+    printf("%d %d %d", get<0>(pq.top()),get<1>(pq.top()),get<2>(pq.top()));
+    int time, wait, answer;
+    answer = 0;
+    time = 0;
+    
     return 0;
 }
