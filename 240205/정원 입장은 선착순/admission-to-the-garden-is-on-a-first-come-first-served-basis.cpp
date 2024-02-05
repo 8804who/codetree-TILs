3726f9af635ad857555a8a8c2b0175e225307eca
README <83859931+8804who@users.noreply.github.com>
2024-02-05 16:11:58 +0900

    Title: 정원 입장은 선착순, Time: 76ms, Memory: 1MB, Status: Passed - CodeTree

--- 240205/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.cpp
+++ 240205/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.cpp
@@ -5,10 +5,15 @@
 
 using namespace std;
 
-struct Comparator {
+struct Comparator1 {
     bool operator()(tuple<int, int, int>& t1, tuple<int, int, int>& t2) {
-        if (get<0>(t1) == get<0>(t2)) return get<2>(t1) < get<2>(t2);
-        else return get<0>(t1) < get<2>(t2);
+        return get<0>(t1) > get<0>(t2);
+     }
+ };
+
+struct Comparator2 {
+    bool operator()(tuple<int, int, int>& t1, tuple<int, int, int>& t2) {
+        return get<2>(t1) > get<2>(t2);
      }
  };
 
@@ -16,25 +21,37 @@ int main() {
     int N, a, t;
     scanf("%d",&N);
 
-    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, Comparator> pq;
+    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, Comparator1> pq1;
+    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, Comparator2> pq2;
+
     for (int i=0;i<N;i++)
     {
         scanf("%d %d",&a, &t);
-        pq.push(make_tuple(a, t, i));
+        pq1.push(make_tuple(a, t, i));
     }
 
-    printf("%d %d %d\n", get<0>(pq.top()),get<1>(pq.top()),get<2>(pq.top()));
-    pq.pop();
-    printf("%d %d %d\n", get<0>(pq.top()),get<1>(pq.top()),get<2>(pq.top()));
-    pq.pop();
-    printf("%d %d %d\n", get<0>(pq.top()),get<1>(pq.top()),get<2>(pq.top()));
-    pq.pop();
-    printf("%d %d %d\n", get<0>(pq.top()),get<1>(pq.top()),get<2>(pq.top()));
-    pq.pop();
-    printf("%d %d %d", get<0>(pq.top()),get<1>(pq.top()),get<2>(pq.top()));
-    int time, wait, answer;
+    int time, answer;
     answer = 0;
-    time = 0;
+    time = get<0>(pq1.top());
     
+    while(true)
+    {
+        if (pq2.empty() && time < get<0>(pq1.top())) time = get<0>(pq1.top());
+        while(!pq1.empty() && time>=get<0>(pq1.top()))
+        {
+            pq2.push(pq1.top());
+            pq1.pop();
+        }
+
+        if (time > get<0>(pq2.top()))
+        {
+            if (answer < time-get<0>(pq2.top())) answer = time-get<0>(pq2.top());
+        }
+        else time = get<0>(pq2.top());
+        time = time + get<1>(pq2.top());
+        pq2.pop();
+        if (pq1.empty() && pq2.empty()) break;
+    }
+    printf("%d", answer);
     return 0;
 }
