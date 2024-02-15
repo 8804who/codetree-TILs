90f31d7900278afb9620fafe12e5f3d9ffad9a58
README <83859931+8804who@users.noreply.github.com>
2024-02-15 16:44:12 +0900

    Title: 점 개수 세기, Time: 565ms, Memory: 30MB, Status: Passed - CodeTree

--- 240215/점 개수 세기/count-number-of-points.py
+++ 240215/점 개수 세기/count-number-of-points.py
@@ -5,6 +5,4 @@ arr = sorted(list(map(int, input().split())))
 
 for _ in range(q):
     s, e = map(int, input().split())
-    left = bisect.bisect_left(arr, s)
-    right = bisect.bisect_right(arr, e)
-    print(right-left)
+    print(bisect.bisect_right(arr, e)-bisect.bisect_left(arr, s))
