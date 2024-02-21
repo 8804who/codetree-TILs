75b671001712f4b84b818f3117b3841a4b13016e
README <83859931+8804who@users.noreply.github.com>
2024-02-21 15:19:37 +0900

    Title: 숫자들의 합이 7의 배수, Time: 200ms, Memory: 26MB, Status: Passed - CodeTree

--- 240221/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.py
+++ 240221/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.py
@@ -1,30 +1,13 @@
 N = int(input())
 
-nums = [int(input()) for _ in range(N)]
-arr1=[0] * N
-arr2=[0] * N
+nums = [0]+[int(input()) for _ in range(N)]
+arr=[1e9] * 7
 
-arr1[0] = nums[0]%7
-for i in range(1, N):
-    arr1[i] = (arr1[i-1]+nums[i])%7
-
-dic = {}
-
-arr2[-1] = nums[-1]%7
-for i in range(N-2, -1, -1):
-    arr2[i] = (arr2[i+1]+nums[i])%7
-
-arr1 = [0]+arr1
-arr2 = arr2+[0]
-
-temp = arr2[0]%7
-
-for i in range(len(arr2)):
-    dic[arr2[i]]=i
-
+n = 0
 answer = 0
-for i in range(len(arr1)):
-    temp2 = temp-arr1[i]
-    if answer < dic[temp2]-i:
-        answer = dic[temp2]-i
+for i in range(1, N+1):
+    n += nums[i]
+    n %= 7
+    answer = max(answer, i-arr[n])
+    arr[n] = min(i,arr[n])
 print(answer)
