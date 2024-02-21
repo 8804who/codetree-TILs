10a762ba0afb9fda77864ec8bff425fcb1c0ecb5
README <83859931+8804who@users.noreply.github.com>
2024-02-21 14:17:56 +0900

    Title: 괄호 쌍 만들어주기, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240221/괄호 쌍 만들어주기/pair-parentheses.py
+++ 240221/괄호 쌍 만들어주기/pair-parentheses.py
@@ -1,16 +1,15 @@
 c = input()
-arr1=[0] * len(c)
-arr2=[0] * len(c)
+
+answer = 0
+arr=[0] * len(c)
+
 for i in range(len(c)-2,-1,-1):
-    arr1[i] = arr1[i+1]
+    arr[i] = arr[i+1]
     if c[i] == ')' and c[i+1] == ')':
-        arr1[i] += 1
+        arr[i] += 1
 
 for i in range(1,len(c)):
     if c[i] == '(' and c[i-1] == '(':
-        arr2[i] += 1
+        answer += arr[i]
 
-answer = 0
-for i in range(len(c)):
-    answer += arr1[i] * arr2[i]
 print(answer)
