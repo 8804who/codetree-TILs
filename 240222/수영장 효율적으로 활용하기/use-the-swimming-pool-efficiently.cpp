ca36693bb1a716fa28d94a3cd6a27c4208943ed8
README <83859931+8804who@users.noreply.github.com>
2024-02-22 15:36:47 +0900

    Title: 수영장 효율적으로 활용하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240222/수영장 효율적으로 활용하기/use-the-swimming-pool-efficiently.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int arr[n];
+
+    for (int i=0;i<n;i++) scanf("%d", &arr[i]);
+    int answer = 1440;
+    int s=1;
+    int e=1440;
+    while(s<=e)
+    {
+        int mid = (s+e)/2;
+        int count = 1;
+        int total = 0;
+        for(int i=0;i<n;i++)
+        {
+            if (mid<total+arr[i]) 
+            { 
+                count++;
+                total=arr[i];
+            }
+            else total+=arr[i];
+        }
+        printf("%d %d\n",mid, count);
+        if (count>m) s=mid+1;
+        else
+        {
+            if (answer>mid) answer = mid;
+            e=mid-1;
+        }
+    }
+
+    printf("%d",answer);
+    return 0;
+}
