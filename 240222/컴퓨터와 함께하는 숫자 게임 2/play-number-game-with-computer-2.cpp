a4e44c245ac026caf3b15332afacfe483cfa0256
README <83859931+8804who@users.noreply.github.com>
2024-02-22 15:21:45 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240222/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.cpp
+++ 240222/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.cpp
@@ -4,33 +4,39 @@
 
 using namespace std;
 
+long long m, a, b;
+long long mid;
+long long s;
+long long e;
+
+int GetTryNum(long long target)
+{
+    s = 1;
+    e = m;
+    int count = 0;
+
+    while (s<=e)
+    {
+        mid = (s+e)/2;
+        count++;
+        if (mid == target) return count;
+        if(mid > target) e=mid-1;
+        else s=mid+1;
+    }
+    return -1;
+}
+
 int main() {
-    long long m, a, b;
-    
     scanf("%d",&m);
     scanf("%d %d",&a, &b);
 
     int min_answer = INT_MAX;
     int max_answer = INT_MIN;
-    int count;
-    long long mid;
-    long long s;
-    long long e;
+    
 
     for (long long i=a;i<=b;i++)
     {
-        s = 1;
-        e = m;
-        count = 0;
-
-        while (s<=e)
-        {
-            mid = (s+e)/2;
-            count++;
-            if (mid == i) break;
-            if(mid > i) e=mid-1;
-            else s=mid+1;
-        }
+        int count = GetTryNum(i);
         min_answer=min(min_answer,count);
         max_answer=max(max_answer,count);
     }
