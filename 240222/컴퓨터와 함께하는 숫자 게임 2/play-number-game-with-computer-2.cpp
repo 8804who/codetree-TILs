600efcb405a26e986986871a19da9630ac3d6fc8
README <83859931+8804who@users.noreply.github.com>
2024-02-22 15:08:29 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 16ms, Memory: 0MB, Status: Passed - CodeTree

--- 240222/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.cpp
+++ 240222/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.cpp
@@ -1,35 +1,51 @@
 #include <iostream>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+// 변수 선언
+long long m;
+long long a, b;
+
+int min_ans = INT_MAX;
+int max_ans = INT_MIN;
+
+int GetTryNum(long long target) {
+    long long left = 1, right = m;
+    int cnt = 0;
+    while (left <= right) {
+        long long mid = (left + right) / 2;
+        cnt++;
+
+        if(mid == target)
+            return cnt;
+        
+        if(mid > target)
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+
+    return -1;
+}
 
 int main() {
-    long long m, a, b;
-    
-    scanf("%d",&m);
-    scanf("%d %d",&a, &b);
+    // 입력
+    cin >> m;
+    cin >> a >> b;
 
-    int min = 1e9;
-    int max = 0;
-    int count;
-    long long mid;
-    long long s;
-    long long e;
-
-    for (int i=a;i<=b;i++)
-    {
-        s = 1;
-        e = m;
-        count = 0;
-
-        while (s<=e)
-        {
-            mid = (s+e)/2;
-            count++;
-            if (mid == i) break;
-            else if(mid > i) e=mid-1;
-            else s=mid+1;
-        }
-        if (count<min) min=count;
-        if (count>max) max=count;
+    // 컴퓨터가 선택한 수를
+    // a부터 b까지 각각 가정해보며
+    // 그 중 최소 횟수와 최대 횟수를 구합니다.
+    for(long long i = a; i <= b; i++) {
+        // 이진탐색을 진행하여 진행 횟수를 구합니다.
+        int num = GetTryNum(i);
+        // 답을 갱신합니다.
+        min_ans = min(min_ans, num);
+        max_ans = max(max_ans, num);
     }
-    printf("%d %d",min,max);
+
+    cout << min_ans << " " << max_ans;
     return 0;
 }
