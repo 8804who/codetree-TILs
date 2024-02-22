c24064ac8fdd72c9744428174aac61f83b68bd4b
README <83859931+8804who@users.noreply.github.com>
2024-02-22 15:28:44 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 16ms, Memory: 0MB, Status: Passed - CodeTree

--- 240222/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.cpp
+++ 240222/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.cpp
@@ -4,20 +4,22 @@
 
 using namespace std;
 
-long long m, a, b, s, e;
 
-int min_answer = INT_MAX;
-int max_answer = INT_MIN;
 
 int main() {
+    int count;
+    long long m, a, b, s, e;
+    int min_answer = INT_MAX;
+    int max_answer = INT_MIN;
+
     scanf("%lld",&m);
     scanf("%lld %lld",&a, &b);
-
+    
     for (long long i=a;i<=b;i++)
     {
         s = 1;
         e = m;
-        int count = 0;
+        count = 0;
 
         while (s<=e)
         {
