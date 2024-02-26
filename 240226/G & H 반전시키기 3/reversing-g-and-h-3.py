276589ab7e4454f9555186ddcb7c46cef95d56ee
README <83859931+8804who@users.noreply.github.com>
2024-02-26 15:41:47 +0900

    Title: G & H 반전시키기 3, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240226/G & H 반전시키기 3/reversing-g-and-h-3.py
@@ -0,0 +1,27 @@
+n = int(input())
+s1 = input()
+s2 = input()
+
+arr = []
+
+for i in range(n):
+    if s1[i]!=s2[i]:
+        arr.append(1)
+    else:
+        arr.append(0)
+
+count = 0
+answer = 0
+for i in range(n):
+    if arr[i] == 1:
+        count += 1
+        if count == 4:
+            answer += 1
+            count = 0
+    else:
+        if count > 0:
+            answer += 1
+            count = 0
+if count > 0:
+    answer += 1
+print(answer)
